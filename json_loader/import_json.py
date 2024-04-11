26122c2e517489473400ae4f94c452c396f90267
Emily Tang <emilytang2002@gmail.com>
2024-04-11 12:06:09 -0400

    populating the seasons and competitions table

--- json_loader/import_json.py
+++ json_loader/import_json.py
@@ -1,3 +1,4 @@
+import psycopg2
 import requests
 
 # import github data only from these seasons
@@ -66,18 +67,142 @@ for comp in valid_comp_data:
 matches_data = get_json_data_from_paths(matches_paths)
 print(f'Fetched matches data from GitHub (length: {len(matches_data)})')
 
-# determine the paths to get the necessary data from lineups and events
-lineups_paths = []
-events_paths = []
+# # determine the paths to get the necessary data from lineups and events
+# lineups_paths = []
+# events_paths = []
+# for match in matches_data:
+#     match_id = match['match_id']
+#     lineups_paths.append(f'lineups/{match_id}.json')
+#     events_paths.append(f'events/{match_id}.json')
+
+# # get the lineups data
+# lineups_data = get_json_data_from_paths(lineups_paths)
+# print(f'Fetched lineups data from GitHub (length: {len(lineups_data)})')
+
+# # get the events data
+# events_data = get_json_data_from_paths(events_paths)
+# print(f'Fetched events data from GitHub (length: {len(events_data)})')
+
+# TESTING ADDING AND CONNECTING TO THE POSTGRESQL DB -------------------------
+
+# connect to PostgreSQL database
+conn = psycopg2.connect(
+    dbname='project_database',
+    user='postgres',
+    password='postgres',
+    host='localhost',
+    port='5432'
+)
+
+# define the CREATE TABLE statement
+CREATE_TABLES_QUERY = '''
+    CREATE TABLE IF NOT EXISTS seasons (
+        season_id INT PRIMARY KEY,
+        season_name VARCHAR(9)
+    );
+    CREATE TABLE IF NOT EXISTS competitions (
+        id SERIAL PRIMARY KEY,
+        competition_id INT,
+        season_id INT,
+        country_name VARCHAR(255),
+        competition_name VARCHAR(255),
+        competition_gender VARCHAR(255),
+        competition_youth BOOLEAN,
+        competition_international BOOLEAN,
+        season_name VARCHAR(9),
+        FOREIGN KEY (season_id) REFERENCES seasons (season_id)
+    );
+    
+'''
+
+# CREATE TABLE IF NOT EXISTS managers (
+#         id INT PRIMARY KEY,
+#         name VARCHAR(255),
+        
+#     );
+
+# CREATE TABLE IF NOT EXISTS teams (
+#         team_id INT PRIMARY KEY,
+#         team_name VARCHAR(255),
+#         team_gender VARCHAR(255),
+#         team_group VARCHAR(255),
+#         country_name VARCHAR(255),
+#         manager_id INT,
+#         FOREIGN KEY (manager_id) REFERENCES managers (manager_id)
+#     );
+
+# CREATE TABLE IF NOT EXISTS matches (
+#         match_id INT PRIMARY KEY,
+#         match_date DATE,
+#         kick_off TIME,
+#         competition_id INT,
+#         season_id INT,
+#         FOREIGN KEY (competition_id) REFERENCES competitions (competition_id),
+#         FOREIGN KEY (season_id) REFERENCES seasons (season_id)
+#     );
+
+INSERT_SEASONS_QUERY = '''
+    INSERT INTO seasons (season_id, season_name)
+    SELECT
+        %s AS season_id,
+        %s AS season_name
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM seasons
+        WHERE season_id = %s
+    );
+'''
+
+INSERT_COMPS_QUERY = '''
+    INSERT INTO COMPETITIONS (competition_id, season_id, country_name,
+    competition_name, competition_gender, competition_youth,
+    competition_international, season_name)
+    SELECT
+        %s AS competition_id,
+        %s AS season_id,
+        %s AS country_name,
+        %s AS competition_name,
+        %s AS competition_gender,
+        %s AS competition_youth,
+        %s AS competition_international,
+        %s AS season_name
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM competitions
+        WHERE competition_id = %s
+    );
+'''
+
+cur = conn.cursor() # a cursor object to execute SQL commands
+
+cur.execute(CREATE_TABLES_QUERY) # execute the CREATE TABLE statement
+conn.commit() # commit the transaction
+
+# prepare and execute the INSERT statement for each item in valid_comp_data
+for comp in valid_comp_data:
+    # populate the seasons table
+    season_id = comp['season_id']
+    seasons_values = (season_id, comp['season_name'], season_id)
+    cur.execute(INSERT_SEASONS_QUERY, seasons_values)
+
+    # populate the competitions table
+    comp_id = comp['competition_id']
+    comp_values = (comp_id, comp['season_id'], comp['country_name'],
+                   comp['competition_name'], comp['competition_gender'],
+                   comp['competition_youth'],
+                   comp['competition_international'], comp['season_name'],
+                   comp_id)
+    cur.execute(INSERT_COMPS_QUERY, comp_values)
+
+print('Populated the seasons table.')
+print('Populated the competitions table.')
+
 for match in matches_data:
-    match_id = match['match_id']
-    lineups_paths.append(f'lineups/{match_id}.json')
-    events_paths.append(f'events/{match_id}.json')
+    print(match)
+    print()
 
-# get the lineups data
-lineups_data = get_json_data_from_paths(lineups_paths)
-print(f'Fetched lineups data from GitHub (length: {len(lineups_data)})')
+conn.commit() # commit the transaction
 
-# get the events data
-events_data = get_json_data_from_paths(events_paths)
-print(f'Fetched events data from GitHub (length: {len(events_data)})')
+# close the cursor and connection
+cur.close()
+conn.close()
